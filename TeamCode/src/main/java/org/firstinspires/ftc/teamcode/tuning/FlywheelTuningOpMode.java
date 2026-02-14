7b22ec6702ce57ac1a10d7543647f1e2f88b750b
KingRainbow44 <kobedo11@gmail.com>
2026-02-14 08:36:56 -0800

    wip: flywheel tuning opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/FlywheelTuningOpMode.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.tuning;
+
+import moe.seikimo.ftc.robot.Robot;
+
+public final class FlywheelTuningOpMode extends Robot {
+    @Override
+    protected OperationMode getOperationType() {
+        return OperationMode.TUNING;
+    }
+}
